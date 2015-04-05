d5602e13f7804c165e5b0ea3fa08837c280513f4
Drogunov Igor <drogunov.igor@gmail.com>
2015-04-05 20:50:59 +0300

    add Lexer::unreadRunes()

--- mol2/lexer.go
+++ mol2/lexer.go
@@ -23,18 +23,32 @@ func (lex *Lexer) Coords() (int, int) {
 	return lex.line, lex.column
 }
 
+func (lex *Lexer) SkipWS() (bool, int) {
+	return false, 0
+}
+
+func (lex *Lexer) unreadRunes(count int) {
+	for i := 0; i < count; i++ {
+		err = lex.buf.UnreadRune()
+		if err != nil {
+			panic(err)
+		}
+	}
+}
+
 func (lex *Lexer) NextAtom() (bool, error) {
 	pattern := "@<TRIPOS>ATOM"
 	err_text := "was expected '" + pattern + "'"
 	if lex.buf.Len() < len(pattern) {
 		return false, errors.New(err_text)
 	}
-	for _, r := range pattern {
+	for count, r := range pattern {
 		buf_rune, _, err := lex.buf.ReadRune()
 		if err != nil {
 			return false, err
 		}
 		if r != buf_rune {
+			lex.unreadRunes(count)
 			return false, errors.New(err_text)
 		}
 	}
